5cfe1addb4add45c3089fd97f780fd8852f6e628
Daniel Pfeiffer <d.pfeiffer404@gmail.com>
2022-03-22 07:02:47 +0100

    reformat

--- api/src/main/scala/jobapi/api/Endpoints.scala
+++ api/src/main/scala/jobapi/api/Endpoints.scala
@@ -7,10 +7,6 @@ import sttp.tapir._
 import sttp.tapir.generic.auto._
 import sttp.tapir.json.play._
 
-import java.time.Instant
-import java.time.temporal.ChronoUnit
-import java.util.UUID
-
 object Endpoints {
 
   implicit val languageSchema: Schema[Language] = Schema.string
@@ -21,76 +17,8 @@ object Endpoints {
     Some(Schema.SName("Map", List("Details")))
   )
 
-  val personExample = Person(
-    id = Some(s"https://id.1brd.com/tenant/${UUID.randomUUID()}/user/${UUID.randomUUID()}"),
-    email = Some("birdy.pigeon@firstbird.com"),
-    firstName = Some("Birdy"),
-    lastName = Some("Pigeon")
-  )
-
-  val jobRequestExample = JobRequest(
-    UUID.randomUUID().toString(),
-    organization = Some(
-      Organization("Firstbird")
-    ),
-    organizationalUnit = Some(
-      OrganizationalUnit("Technology Swarm")
-    ),
-    contact = Some(personExample),
-    responsible = List(personExample),
-    validity = Some(
-      Validity(
-        from = Some(Instant.now().minus(1, ChronoUnit.DAYS)),
-        to = Some(Instant.now().plus(1, ChronoUnit.DAYS))
-      )
-    ),
-    locations = List(
-      Location(
-        name = "Firstbird HQ",
-        // address = Some("Gertrude-Fröhlich-Sandner Straße 2-4, Tower 9, Spaces"),
-        municipality = Some("Vienna"),
-        countryCode = Some("AUT"),
-        coordinates = Some(Coordinates(longitude = 48.186618, latitude = 16.375152))
-      )
-    ),
-    externalId = Some("123"),
-    links = JobLinks(
-      posting = Some(Url("https://localhost")),
-      `apply` = Some(Url("https://localhost"))
-    ),
-    metadata = Map(
-      "x-firstbird-job-parsing" -> Map(
-        "a" -> "b"
-      )
-    ),
-    attributes = JobAttributes(
-      categories = List(Category("Trainee")),
-      experience = Some(Experience("senior")),
-      schedule = Some(Schedule("full-time")),
-      remote = Some(Remote("onsite")),
-      keywords = List(
-        Keyword("Java", 0.99),
-        Keyword("Scala", 0.95)
-      )
-    ),
-    localization = Map(
-      Language("de") -> JobDetails(
-        "Software Developer",
-        skills = List(Skill("Java")),
-        description = Description(
-          html = Some(Html("<h1>Requirements</h1><ul><li>Java</li></ul>")),
-          parts = List(
-            DescriptionPart(name = "requirements",
-                            title = Some("Requirements"),
-                            html = Some(Html("<ul><li>Java</li></ul>")))
-          )
-        )
-      )
-    )
-  )
-
   val getJob = endpoint.get
     .in("jobs")
-    .out(jsonBody[JobRequest].example(jobRequestExample))
+    .out(jsonBody[JobRequest].example(JobRequestExample.value))
 
 }
